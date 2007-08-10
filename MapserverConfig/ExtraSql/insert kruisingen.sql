ae282e41c124ddbe598cbfabbdbb853aa2c0ad57
Roy Braam <braam.roy@gmail.com>
2007-08-10 14:29:08 +0000

    Kruisingen query wat aangepast. Map file aangepast zodat hij werkt op de view alg_kom_10_wgw_v ipv tabel. een extra sql voor vri's koppelen aan kruispunten. Voert wat querys uit zodat ongeveer de zelfde namen worden gekoppeld aan de kruispunten.

--- MapserverConfig/ExtraSql/insert kruisingen.sql
+++ MapserverConfig/ExtraSql/insert kruisingen.sql
@@ -1,7 +1,7 @@
 DELETE FROM kruisingen;
 INSERT INTO kruisingen(
             kruisende_straatnaam, the_geom)
-    select n.stt_naam, multi(endpoint(n.the_geom)) from nwb n 
+    select lower(n.stt_naam), multi(endpoint(n.the_geom)) from nwb n 
 	where lower(n.wegbehsrt) = 'g' and
 		(Touches(endpoint(n.the_geom),(select n1.the_geom from nwb n1 
 			where lower(n1.wegbehsrt) = 'p'
@@ -9,7 +9,7 @@ INSERT INTO kruisingen(
 
 INSERT INTO kruisingen(
             kruisende_straatnaam, the_geom)
-    select n.stt_naam, multi(startpoint(n.the_geom)) from nwb n 
+    select lower(n.stt_naam, multi(startpoint(n.the_geom)) from nwb n 
 	where 
 		-- NIET DE ZELFDE PUNTEN. MAAR UITGEZET OMDAT WE DE NAMEN NIET WETEN. DUS MOETEN WEL ALLEMAAL GEMAAKT WORDEN ZODAT ALLE MOGELIJKE NAMEN VOOR EEN KRUISPUNT ER IN STAAN.
 		--NOT multi(startpoint(n.the_geom)) ~= (select n2.the_geom from nwb n2 order by distance(startpoint(n.the_geom),n2.the_geom) limit 1)and
@@ -17,3 +17,6 @@ INSERT INTO kruisingen(
 		(Touches(startpoint(n.the_geom),(select n1.the_geom from nwb n1 
 			where lower(n1.wegbehsrt) = 'p'
 			order by distance(n.the_geom,n1.the_geom) limit 1)) );
+
+UPDATE kruisingen k
+   SET prov_wegnr= (select n.wegnummer from nwb n where n.wegbehsrt='P' and TOUCHES(k.the_geom,n.the_geom) LIMIT 1);

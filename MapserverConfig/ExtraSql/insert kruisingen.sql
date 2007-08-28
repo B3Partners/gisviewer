d32905c4a44b9d32854c66e8eb13bd0c7bf74cb3
Roy Braam <braam.roy@gmail.com>
2007-08-28 14:11:06 +0000

    Alle kruisingen worden nu aangemaakt in sql. Mapfile performance puntje

--- MapserverConfig/ExtraSql/insert kruisingen.sql
+++ MapserverConfig/ExtraSql/insert kruisingen.sql
@@ -1,6 +1,7 @@
 DELETE FROM kruisingen;
+-- Voor alle kruisingen tussen rijkswegen en provincie wegen
 INSERT INTO kruisingen(
-            kruisende_straatnaam, the_geom)
+            zijweg_naam, the_geom)
     select lower(n.stt_naam), multi(endpoint(n.the_geom)) from nwb n 
 	where lower(n.wegbehsrt) = 'g' and
 		(Touches(endpoint(n.the_geom),(select n1.the_geom from nwb n1 
@@ -8,12 +9,32 @@ INSERT INTO kruisingen(
 			order by distance(n.the_geom,n1.the_geom) limit 1)) );
 
 INSERT INTO kruisingen(
-            kruisende_straatnaam, the_geom)
-    select lower(n.stt_naam, multi(startpoint(n.the_geom)) from nwb n 
+            zijweg_naam, the_geom)
+    select lower(n.stt_naam), multi(startpoint(n.the_geom)) from nwb n 
 	where 
 		-- NIET DE ZELFDE PUNTEN. MAAR UITGEZET OMDAT WE DE NAMEN NIET WETEN. DUS MOETEN WEL ALLEMAAL GEMAAKT WORDEN ZODAT ALLE MOGELIJKE NAMEN VOOR EEN KRUISPUNT ER IN STAAN.
 		--NOT multi(startpoint(n.the_geom)) ~= (select n2.the_geom from nwb n2 order by distance(startpoint(n.the_geom),n2.the_geom) limit 1)and
 		lower(n.wegbehsrt) = 'g' and
+		(Touches(startpoint(n.the_geom),(select n1.the_geom from nwb n1 
+			where lower(n1.wegbehsrt) = 'p'
+			order by distance(n.the_geom,n1.the_geom) limit 1)) );
+
+-- Voor alle kruisingen tussen rijkswegen en provincie wegen
+INSERT INTO kruisingen(
+            zijweg_naam, the_geom)
+    select 'a'||n.wegnummerint, multi(endpoint(n.the_geom)) from nwb n 
+	where lower(n.wegbehsrt) = 'r' and
+		(Touches(endpoint(n.the_geom),(select n1.the_geom from nwb n1 
+			where lower(n1.wegbehsrt) = 'p'
+			order by distance(n.the_geom,n1.the_geom) limit 1)) );
+
+INSERT INTO kruisingen(
+            zijweg_naam, the_geom)
+    select 'a'||n.wegnummerint, multi(startpoint(n.the_geom)) from nwb n 
+	where 
+		-- NIET DE ZELFDE PUNTEN. MAAR UITGEZET OMDAT WE DE NAMEN NIET WETEN. DUS MOETEN WEL ALLEMAAL GEMAAKT WORDEN ZODAT ALLE MOGELIJKE NAMEN VOOR EEN KRUISPUNT ER IN STAAN.
+		--NOT multi(startpoint(n.the_geom)) ~= (select n2.the_geom from nwb n2 order by distance(startpoint(n.the_geom),n2.the_geom) limit 1)and
+		lower(n.wegbehsrt) = 'r' and
 		(Touches(startpoint(n.the_geom),(select n1.the_geom from nwb n1 
 			where lower(n1.wegbehsrt) = 'p'
 			order by distance(n.the_geom,n1.the_geom) limit 1)) );

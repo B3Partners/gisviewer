6c343a0731dcb49c086e30966b7ea716b6fd8aa8
Roy Braam <braam.roy@gmail.com>
2007-08-31 14:05:44 +0000

    Een begin aan 2 functies voor het maken van de vlekken. Er wordt nu alleen maar gekeken of de 1 met de ander intersects.

--- MapserverConfig/ExtraSql/insert vlekkenkaart 2004 2006.sql
+++ MapserverConfig/ExtraSql/insert vlekkenkaart 2004 2006.sql
@@ -1,5 +1,5 @@
-INSERT INTO vlekkenkaart2004_2006 v(the_geom)
-	select buffer(new.the_geom,20) from dgn new where Disjoint(new.the_geom,(select old.the_geom from v7 old order by distance (new.the_geom,old.the_geom) limit 1));
+INSERT INTO vlekkenkaart2004_2006 (the_geom)
+	select buffer(n.the_geom,20) from dgn n where Disjoint(n.the_geom,(select o.the_geom from v7 o order by distance (n.the_geom,o.the_geom) limit 1));
 
-INSERT INTO vlekkenkaart2004_2006 v(the_geom)
-	select buffer(old.the_geom,20) from v7 old where Disjoint(old.the_geom,(select new.the_geom from dgn new order by distance (new.the_geom,old.the_geom) limit 1));
+--INSERT INTO vlekkenkaart2004_2006 (the_geom)
+--	select buffer(o.the_geom,20) from v7 o where Disjoint(o.the_geom,(select n.the_geom from dgn n order by distance (n.the_geom,o.the_geom) limit 1));

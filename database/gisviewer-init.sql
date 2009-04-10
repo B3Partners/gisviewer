77f2e0ee294666950325abf7f3ebfff51926fc6d
Matthijs Laan <matthijsln@gmail.com>
2009-04-10 16:29:33 +0000

    juiste init script

--- database/gisviewer-init.sql
+++ database/gisviewer-init.sql
@@ -1,17 +1,11 @@
-INSERT INTO moscow (id, code, naam) VALUES ('1','M','Must Have');
-INSERT INTO moscow (id, code, naam) VALUES ('2','S','Should Have');
-INSERT INTO moscow (id, code, naam) VALUES ('3','C','Could Have');
-INSERT INTO moscow (id, code, naam) VALUES ('4','W','Will Not Have');
-INSERT INTO moscow (id, code, naam) VALUES ('5','O','Onbekend');
-
-INSERT INTO data_typen (id, naam) VALUES ('1','data');
-INSERT INTO data_typen (id, naam) VALUES ('2','url');
-INSERT INTO data_typen (id, naam) VALUES ('3','query');
+INSERT INTO data_typen (id, naam) VALUES (1,'data');
+INSERT INTO data_typen (id, naam) VALUES (2,'url');
+INSERT INTO data_typen (id, naam) VALUES (3,'query');
 INSERT INTO data_typen (id, naam) VALUES (4, 'Javascript functie');
 
-INSERT INTO waarde_typen (id, naam) VALUES ('1','string');
-INSERT INTO waarde_typen (id, naam) VALUES ('2','integer');
-INSERT INTO waarde_typen (id, naam) VALUES ('3','double');
-INSERT INTO waarde_typen (id, naam) VALUES ('4','date');
-INSERT INTO waarde_typen (id, naam) VALUES ('5','url');
-insert into waarde_typen (id, naam) values ('6','boolean');
+INSERT INTO waarde_typen (id, naam) VALUES (1,'string');
+INSERT INTO waarde_typen (id, naam) VALUES (2,'integer');
+INSERT INTO waarde_typen (id, naam) VALUES (3,'double');
+INSERT INTO waarde_typen (id, naam) VALUES (4,'date');
+INSERT INTO waarde_typen (id, naam) VALUES (5,'url');
+insert into waarde_typen (id, naam) values (6,'boolean');

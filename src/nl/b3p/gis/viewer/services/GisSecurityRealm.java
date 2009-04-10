2a30d09c080c87b31c5071803de84561bf94e718
Matthijs Laan <matthijsln@gmail.com>
2009-04-10 16:30:02 +0000

    log stacktrace

--- src/nl/b3p/gis/viewer/services/GisSecurityRealm.java
+++ src/nl/b3p/gis/viewer/services/GisSecurityRealm.java
@@ -103,7 +103,7 @@ public class GisSecurityRealm implements FlexibleRealmInterface, ExternalAuthent
         try {
             sp = wmscr.getProvider(location, username, password);
         } catch (Exception ex) {
-            log.error(ex.getLocalizedMessage());
+            log.error("Error reading GetCapabilities", ex);
         }
 
         if (sp == null) {

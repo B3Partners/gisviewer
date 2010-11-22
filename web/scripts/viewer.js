0bfea4e069172eed37b0885586f550e4eb02f03c
Roy Braam <braam.roy@gmail.com>
2010-11-22 16:20:16 +0000

    Tool meganisme toegevoegd aan Controller nog niet getest.

--- web/scripts/viewer.js
+++ web/scripts/viewer.js
@@ -40,6 +40,7 @@ if (mapviewer== "flamingo"){
     var opt={
         projection:"EPSG:28992",
         maxExtent: new OpenLayers.Bounds(0, 304000, 280000, 628000),
+        allOverlays: true,
         resolutions: [512,256,128,64,32,16,8,4,2,1,0.5,0.25,0.125]
     };
     $j("#mapcontent").html(" ");

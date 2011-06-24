b16f147c27f062edc777fb790df69f6d7816581b
Boy de Wit <boydewit@b3partners.nl>
2011-06-24 14:49:16 +0000

    - Bezig met vinkjes naast de kaartselectie te submitten naar back-end via html:mutlibox

--- web/WEB-INF/jsp/viewer/kaartselectie.jsp
+++ web/WEB-INF/jsp/viewer/kaartselectie.jsp
@@ -1,31 +1,49 @@
 <%@include file="/WEB-INF/jsp/taglibs.jsp" %>
 <%@ page isELIgnored="false"%>
 
+<c:set var="form" value="${kaartselectieForm}"/>
+
 <script type="text/javascript" src='dwr/interface/JMapData.js'></script>
 <script type="text/javascript" src='dwr/interface/JCollectAdmindata.js'></script>
 <script type="text/javascript" src='dwr/engine.js'></script>
+
 <script type="text/javascript" src="<html:rewrite page='/scripts/kaartselectie.js'/>"></script>
+<script type="text/javascript" src="<html:rewrite page="/scripts/simple_treeview.js"/>"></script>
 
 <tiles:insert definition="specialMessages"/>
 
 <h2>Kaartselectie</h2>
 
-<p>User kaartgroepen</p>
+<p>Vaste kaartlagen</p>
 
-<table>
-    <tr>
-        <td>ID</td>
-        <td>Code</td>
-        <td>Cluster ID</td>
-        <td>Default On</td>
-    </tr>
+<html:form styleId="kaartselectieForm" action="/kaartselectie">
+    <div id="layermaindiv"></div>
 
-    <c:forEach var="groep" items="${user_kaartgroepen}">
-        <tr>
-            <td><c:out value="${groep.id}" /></td>
-            <td><c:out value="${groep.code}" /></td>
-            <td><c:out value="${groep.clusterid}" /></td>
-            <td><c:out value="${groep.default_on}" /></td>
-        </tr>
-    </c:forEach>
-</table>
+    <html:submit property="save">Opslaan</html:submit>
+</html:form>
+
+<script type="text/javascript">
+    function catchEmpty(defval){
+        return defval
+    }
+    
+    var themaTree = catchEmpty(${tree});
+    if(typeof themaTree === 'undefined' || !themaTree) {
+        themaTree = null;
+    }
+
+    treeview_create({
+        "id": "layermaindiv",
+        "root": themaTree,
+        "rootChildrenAsRoots": true,
+        "itemLabelCreatorFunction": createLeaf,
+        "toggleImages": {
+            "collapsed": "<html:rewrite page="/images/treeview/plus.gif"/>",
+            "expanded": "<html:rewrite page="/images/treeview/minus.gif"/>",
+            "leaf": "<html:rewrite page="/images/treeview/leaft.gif"/>"
+        },
+        "saveExpandedState": true,
+        "streeaveScrollState": true,
+        "expandAll": false
+    }); 
+</script>
--- web/scripts/kaartselectie.js
+++ web/scripts/kaartselectie.js
@@ -1,9 +1,83 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+/* kaartselectie functies */
+var kaartgroepenAan = new Array();
+var kaartlagenAan = new Array();
 
-function test() {
-    alert('test');
+function createLeaf(container, item) {
+    container.appendChild(document.createTextNode(' '));
+    container.appendChild(document.createTextNode(item.title));
+
+    if (item.cluster)
+        container.appendChild(createCheckboxClusterStruts(item, false));
+    else
+        container.appendChild(createCheckboxThema(item, false));
+
+    return false;
 }
 
+function createCheckboxCluster(item, checked){
+
+    var checkbox;
+    if (ieVersion <= 8 && ieVersion != -1) {
+        var checkboxControleString = '<input name="kaartgroepenAan" type="checkbox" id="' + item.id + '"';
+        if (checked) {
+            checkboxControleString += ' checked="checked"';
+        }
+        checkboxControleString += ' value="' + item.id + '" onclick="clusterClick(this)"';
+        checkboxControleString += '>';
+        checkbox = document.createElement(checkboxControleString);
+    }else{
+        checkbox = document.createElement('input');
+        checkbox.id = item.id;
+        checkbox.type = 'checkbox';
+        checkbox.value = item.id;
+        checkbox.onclick = function(){
+            clusterClick(this);
+        }
+        if(checked) {
+            checkbox.checked = true;
+        }
+    }
+
+    return checkbox;
+}
+
+function createCheckboxClusterStruts(item, checked){
+    var checkbox;
+
+    checkbox = document.createElement('html:multibox');
+    checkbox.id = item.id;
+    checkbox.property = 'kaartgroepenAan';
+    checkbox.value = item.id;
+
+    return checkbox;
+}
+
+function createCheckboxThema(item, checked) {
+    var checkbox;
+
+    if (ieVersion <= 8 && ieVersion != -1) {
+
+        var checkboxControleString = '<input name="kaartlagenAan" type="checkbox" id="' + item.id + '"';
+        if (checked) {
+            checkboxControleString += ' checked="checked"';
+        }
+        checkboxControleString += ' value="' + item.id + '" onclick="themaClick(this)"';
+        checkboxControleString += '>';
+        checkbox = document.createElement(checkboxControleString);
+
+    } else {
+        checkbox = document.createElement('input');
+        checkbox.id = item.id;
+        checkbox.type = 'checkbox';
+        checkbox.value = item.id;
+
+        checkbox.onclick = function() {
+            themaClick(this);
+        }
+
+        if (checked) {
+            checkbox.checked = true;
+        }
+    }
+    return checkbox;
+}

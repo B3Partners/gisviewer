b65fb5243c4530dc568903ccf181af0f6a0951d9
Boy de Wit <boydewit@b3partners.nl>
2012-07-27 15:01:27 +0000

    - other icon for gps in openlayers; - deleted other gps icon in ol;
    todo: fix gps button in viewer; does toggle work in openlayers ?
    
    
    
    Former-commit-id: 8982549d145ff88ad49ec27b551f084244b034e1

--- web/scripts/GPSComponent.js
+++ web/scripts/GPSComponent.js
@@ -16,7 +16,7 @@
  */
 
 
-function GPSComponent(buffer){
+function GPSComponent(buffer){    
     this.timeoutId = null;
     this.interval = 20000;
     if(buffer){
@@ -27,7 +27,7 @@ function GPSComponent(buffer){
     Proj4js.defs["EPSG:28992"] = "+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +towgs84=565.237,50.0087,465.658,-0.406857,0.350733,-1.87035,4.0812 +units=m +no_defs";
     Proj4js.defs["EPSG:4236"] = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs ";
     
-    this.startPolling = function (){
+    this.startPolling = function (){        
         // Request repeated updates.
         gpsComponent.timeoutId = setInterval(gpsComponent.once,gpsComponent.interval);
         gpsComponent.once();
